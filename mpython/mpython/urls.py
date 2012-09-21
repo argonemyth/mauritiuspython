74e38c8b47917a9a09e0320d2901c93737f371b4
Fei Tan <fei@argonemyth.com>
2012-09-21 12:42:48 +0400

    Added about page; content for the about page has not completed yet.

--- mpython/mpython/urls.py
+++ mpython/mpython/urls.py
@@ -4,12 +4,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from views import HomeView
+from views import HomeView, AboutView
 
 urlpatterns = patterns('',
     # Examples:
     #url(r'^$', 'mpython.views.home', name='home'),
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^about/$', AboutView.as_view(), name='about'),
     # url(r'^mpython/', include('mpython.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- mpython/mpython/views.py
+++ mpython/mpython/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "base/landing.html"
+
+class AboutView(TemplateView):
+    template_name = "about.html"
