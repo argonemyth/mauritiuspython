fe008a4f73b1dd868356d5d020a9fdcde6faca25
Fei Tan <fei@argonemyth.com>
2012-09-23 11:47:57 +0400

    Fixed some typos and syntax in the admin.

--- mpython/mailblast/admin.py
+++ mpython/mailblast/admin.py
@@ -13,7 +13,7 @@ class NewsletterAdmin(admin.ModelAdmin):
                     'sender_email', 'date_create', 'date_modify')
     list_editable = ('active', 'newsletter_type', 'sender_name', 'sender_email')
     list_filter = ('active', 'newsletter_type')
-    date_hierarchy = "data_create"
+    date_hierarchy = "date_create"
     ordering = ("-date_create", )
 
 
@@ -22,10 +22,10 @@ class SubscriptionAdmin(admin.ModelAdmin):
     Admin class for subscription.
     """
     list_display = ('__unicode__', 'subscribed', 'date_create')
-    list_editable = ('subscribed')
-    list_filter = ('subscribed')
-    search_fields = ('email')
-    date_hierarchy = 'data_create'
+    list_editable = ('subscribed', )
+    list_filter = ('subscribed', )
+    search_fields = ('email', )
+    date_hierarchy = 'date_create'
     ordering = ('date_create', )
 
 
@@ -37,7 +37,7 @@ class EmailAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'composer', 'status', 'date_create', 
                     'date_modify',)
     list_filter = ('status', )
-    date_hierarchy = "data_create"
+    date_hierarchy = "date_create"
     ordering = ("-date_create", )
 
 admin.site.register(Newsletter, NewsletterAdmin)
