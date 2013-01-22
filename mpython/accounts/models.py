c39f63ba7c04e8e04a83035d27da5fbd05aa6415
Fei Tan <fei@argonemyth.com>
2013-01-22 15:17:35 +0400

    Added extra fields for user profile.

--- mpython/accounts/models.py
+++ mpython/accounts/models.py
@@ -4,7 +4,27 @@ from django.utils.translation import ugettext_lazy as _
 from userena.models import UserenaLanguageBaseProfile 
 
 class Profile(UserenaLanguageBaseProfile):
+    GENDER_CHOICES = ( 
+        (1, _('Male')),
+        (2, _('Female')),
+    ) 
+
     user = models.OneToOneField(User,
                                 unique=True,
                                 verbose_name=_('user'),
                                 related_name='profile')
+    bio = models.TextField(_('bio'), blank=True, null=True)
+    show_email = models.BooleanField(_('show email?'), blank=True, default=False,
+                                     help_text= _("Don't select it if you don't\
+                                      want to show your email on the profile."))
+    website = models.URLField(_('website'), blank=True, verify_exists=False)
+    irc_nick = models.CharField(_('IRC nick'), blank=True, null=True,
+                                max_length=100, help_text=_("on freenode!"))
+    twitter = models.CharField(_('Twitter username'), max_length=100,
+                               blank=True, null=True)
+    skype_nick = models.CharField(_('Skype nick'), max_length=100, blank=True,
+                                  null=True)
+    gender = models.PositiveSmallIntegerField(_('gender'),
+                                              choices=GENDER_CHOICES,
+                                              blank=True, null=True)
+    birth_date = models.DateField(_('birth date'), blank=True, null=True)
