e05104b66055703618c0f0d5d5aacb238fe63092
Fei Tan <fei@argonemyth.com>
2013-01-22 09:36:44 +0400

    Changed the widgets on the landing page.

--- mpython/mpython/views.py
+++ mpython/mpython/views.py
@@ -1,7 +1,19 @@
 from django.views.generic import TemplateView
+from schedule.models import Event
+
 
 class HomeView(TemplateView):
     template_name = "base/landing.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(HomeView, self).get_context_data(**kwargs)
+        # TODO: Add site activities
+        context['recent_activities'] = None
+        # Add recent events
+        context['recent_events'] = Event.objects.filter().order_by('-start')[:5]
+        # Add in recent twitter stream 
+        return context
 
 class AboutView(TemplateView):
     template_name = "about.html"
