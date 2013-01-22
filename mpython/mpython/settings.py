d67e396f14b12cdc293b4654853eafe0ed3d8427
Fei Tan <fei@argonemyth.com>
2013-01-22 12:03:23 +0400

    Changed static and media url.

--- mpython/mpython/settings.py
+++ mpython/mpython/settings.py
@@ -40,7 +40,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/var/www/media/mpython.com/'
+MEDIA_ROOT = '/var/www/media/mpython/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -51,7 +51,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/static/mpython.com/'
+STATIC_ROOT = '/var/www/static/mpython/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
